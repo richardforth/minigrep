61ae17e69c70143c02ba33af9fd4488b887c72ce
Richard A. Forth <richard.forth@gmail.com>
2025-10-01 23:24:52 +0000

    Improve error handling when wrong number of (or no) arguments passed.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("E: Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     println!("Searching for query: {}", config.query);
     println!("In file: {}", config.file_path);
@@ -22,10 +26,18 @@ struct Config {
 
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    // Many developers expect 'new' functions to never fail, so moved to 'build' instead
+    // We either return a Config, or an error string with lifetime 'static
+    // Our error values will always be string literals with the 'static lifetime.
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        // improve 'index out of bounds' error to something more meaningful
+        if args.len() < 3  { // 3 because script path, arg1, arg2
+            return Err("Not enough arguments, 2 required.");
+        }
+
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Config { query, file_path }
+        Ok(Config { query, file_path })
     }
 }
