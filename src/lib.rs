69808d5c7bf3a0df5ee432034a35fa7d0175affb
Richard A. Forth <richard.forth@gmail.com>
2025-10-02 19:12:03 +0000

    Added new function into run function, and added code to support case insensitive search via an environment variable

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,18 @@
 use std::fs;
+use std::env;
 use std::error::Error;
 
 // Return a Result<T, E> when something goes wrong
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    for line in search(&config.query, &contents) {
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{line}");
     }
 
@@ -15,13 +22,16 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 pub struct Config {
     pub query: String,
     pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config {
     // Many developers expect 'new' functions to never fail, so moved to 'build' instead
     // We either return a Config, or an error string with lifetime 'static
     // Our error values will always be string literals with the 'static lifetime.
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+    pub fn build(
+        args: &[String]
+    ) -> Result<Config, &'static str> {
         // improve 'index out of bounds' error to something more meaningful
         if args.len() < 3  { // 3 because script path, arg1, arg2
             return Err("Not enough arguments, 2 required.");
@@ -30,7 +40,13 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Ok(Config { query, file_path })
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case
+        })
     }
 }
 
