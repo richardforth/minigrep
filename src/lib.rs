a9242833a1b1eb21882665836974b3b85efc130a
Richard A. Forth <richard.forth@gmail.com>
2025-10-02 16:44:56 +0000

    Added text to passing test for case-sensitivity, which will not be picked up, even though the test passes, added a separate test that will fail, so we can add code later to cope with case sensitivity

--- src/lib.rs
+++ src/lib.rs
@@ -63,10 +63,26 @@ mod tests {
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape.";
 
         assert_eq!(
             vec!["safe, fast, productive."],
+            search(query, contents)
+        );
+    }
+
+    #[test]
+    fn case_sensitive() {
+        let query = "RuSt";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust", "Trust me.."],
             search(query, contents)
         );
     }
